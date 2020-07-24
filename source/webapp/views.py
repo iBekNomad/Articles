9bce25b0dd44de177a2e100ec22a0a74a0ab54c9
Aibek <nomad.aibek@gmail.com>
2020-07-24 11:38:33 +0600

    Added 'adding post' to posts

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -13,12 +13,11 @@ def article_create_view(request):
     if request.method == 'GET':
         return render(request, 'article_create.html')
     elif request.method == 'POST':
-        print(request.POST)
-        context = {
-            'title': request.POST.get('title'),
-            'content': request.POST.get('content'),
-            'author': request.POST.get('author')
-        }
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        author = request.POST.get('author')
+        article = Article.objects.create(title=title, text=text, author=author)
+        context = {'article': article}
         return render(request, 'article_view.html', context)
 
 
